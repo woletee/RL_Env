3777250b0bc76ee37ad4735432bc05c5bafa4240
Liyew <118590747+woletee@users.noreply.github.com>
2024-01-25 16:27:14 +0900

    Update Q_learning.py

--- Q_learning.py
+++ Q_learning.py
@@ -6,3 +6,13 @@ import pickle
 from matplotlib import style
 import time 
 
+style.use("ggplot")
+
+SIZE=10
+HM_EPISODES=25000
+MOVE_PENALITY=1
+ENEMY_PENALTY=300
+FOOD_REWARD=25
+epsilon=0.9
+EPS_DECAY=0.9998
+SHOW_EVERY=3000
